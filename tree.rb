d83d9c941dc90192ddcaff5b4e410c4cdbbe5569
Eveanandi Butler & Lauren N. Roth <pair+ebutler90.laurennicoleroth@devbootcamp.com>
2015-02-24 16:35:11 -0500

    Add files for testing recursive approach to printing the contents of any directory

--- tree.rb
+++ tree.rb
@@ -4,13 +4,31 @@
 #   puts "./" + path
 # end
 
-#Release 2
-current_contents = Dir.glob("*")
-current_contents.each do |path|
-  puts "./" + path
-  files = Dir.glob("#{path}/**/*")
-  files.each do |file|
-    puts "./" + file
+#Release 2: iterative approach prints out correct output, without formatting
+# current_contents = Dir.glob("./*")
+# current_contents.each do |path|
+#   puts path
+#   files = Dir.glob("#{path}/**/*")
+#   files.each do |file|
+#     puts file
+#   end
+# end
+
+#trying to change iterative approach to an recursive approach:
+
+def print_contents(directory)
+  current_contents = Dir.glob("#{directory}/*")
+  puts "."
+  current_contents.each do |path|
+    puts "  " + path
+    # print_contents(path)
+    children = Dir.glob("#{path}/*")
+    children.each do |child|
+      print_contents(child)
+    end
   end
 end
+
+print_contents("/Users/apprentice/tree-utility-challenge")
+
 
