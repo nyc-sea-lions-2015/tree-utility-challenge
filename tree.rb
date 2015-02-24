f94c24a575c6655f16fc405665e75c2d9fc4e4df
Eveanandi Butler & Lauren N. Roth <pair+ebutler90.laurennicoleroth@devbootcamp.com>
2015-02-24 15:52:55 -0500

    Add release 2 which recursively prints the tree of the current directory

--- tree.rb
+++ tree.rb
@@ -1,5 +1,16 @@
+#Release 1
+# current_contents = Dir.glob("*")
+# current_contents.each do |path|
+#   puts "./" + path
+# end
 
+#Release 2
 current_contents = Dir.glob("*")
-current_contents.each do |item|
-  puts "./" + item
+current_contents.each do |path|
+  puts "./" + path
+  files = Dir.glob("#{path}/**/*")
+  files.each do |file|
+    puts "./" + file
+  end
 end
+
