fc4060b2e9216bf9695e6fc907bbe7dfc9728c93
Lucian Kahn <luciankahn@gmail.com>
2015-03-17 15:33:08 -0400

    fixed tabs

--- tree.rb
+++ tree.rb
@@ -24,20 +24,13 @@
 class Tree
   attr_accessor :counter
 
-  def initialize
-    @counter = 1
-  end
-
-  def show_tree(path)
+  def show_tree(path, counter)
     Dir[path + '/*'].each do |sub_directory|
-      puts ("\t" * counter) + sub_directory
-      if File.directory?(sub_directory) # if it's a directory and not just a file
-        @counter += 1
-        show_tree(sub_directory)
-      end
+      puts ("   " * counter) + sub_directory
+      show_tree(sub_directory, counter+1)
     end
   end
 end
 
 test = Tree.new
-test.show_tree('.')
+test.show_tree('.', 0)
