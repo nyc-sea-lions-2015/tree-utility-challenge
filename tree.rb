479f27112c8851e7c3ccb8e9a79a6a7d19568f72
Lucian Kahn <luciankahn@gmail.com>
2015-03-17 15:55:00 -0400

    complete release 4

--- tree.rb
+++ tree.rb
@@ -21,20 +21,37 @@
 
 # Release 3
 
-class Tree
-  attr_accessor :counter
+# class Tree
+#   attr_accessor :counter
 
-  def show_tree(path, counter) # passing the counter as a parameter to the method instead of using a global variable
-                               # prevents the counter from going up and up forever.
-    Dir[path + '/*'].each do |sub_directory|
-      puts ("   " * counter) + File.basename(sub_directory)
-      show_tree(sub_directory, counter+1)
-    end
+#   def show_tree(path, counter) # passing the counter as a parameter to the method instead of using a global variable
+#                                # prevents the counter from going up and up forever.
+#     Dir[path + '/*'].each do |sub_directory|
+#       puts ("   " * counter) + File.basename(sub_directory)
+#       show_tree(sub_directory, counter+1)
+#     end
+#   end
+# end
+
+# test = Tree.new
+# test.show_tree('.', 0)  # passing the counter as a parameter to the method instead of using a global variable
+#                         # prevents the counter from going up and up forever.
+
+
+# Release 4
+
+def show_tree(path, counter=0) # passing the counter as a parameter to the method instead of using a global variable
+                             # prevents the counter from going up and up forever.
+  Dir[path + '/*'].each do |sub_directory|
+    puts ("   " * counter) + File.basename(sub_directory)
+    show_tree(sub_directory, counter+1)
   end
 end
 
-test = Tree.new
-test.show_tree('.', 0)  # passing the counter as a parameter to the method instead of using a global variable
-                        # prevents the counter from going up and up forever.
+if ARGV.length > 0
+  path = ARGV[0]
+else
+  path = '.'
+end
 
-
+show_tree(path)
