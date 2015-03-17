3540f5c671dcdd011dd2567401916485d83acc62
Carl Swanson & dmilburn <pair+cetswanson.dmilburn@devbootcamp.com>
2015-03-17 18:53:06 -0400

    Update tree utility through release 4

--- tree.rb
+++ tree.rb
@@ -1,53 +1,17 @@
-# def tree
-#   return Dir.glob('./**')
-#   return Dir.glob('./*/*')
-# end
-
-# def tree
-# Dir.glob('./*').each {|x|
-#   if File.directory?(x) == true
-#     "shoe"
-#   else
-#     "nothing"
-#   end
-#   }
-# end
-
-# puts tree
-
-
-def tree
-  y = ("./*")
+def tree(arg = ARGV, indent = "")
+  if arg.is_a?(Array)
+    y = arg.join("") + "/*"
+  else
+    y = ("./*")
+  end
   Dir[y].each {|x|
-    puts Dir.getwd + "/" + File.basename(x)
+    puts indent + "|--" + File.basename(x)
     if File.directory?(x) == true
       Dir.chdir(x)
-      tree
+      tree(y, indent += " ")
+      indent[0] = ""
       Dir.chdir('../')
     end}
 end
 
-# tree
-
-# def tree
-#   filepath = "."
-#   y = ("./*")
-#   Dir[y].each {|x|
-#     if File.directory?(x) == true
-#       #filepath << File.basename(x)
-#       Dir.chdir(x)
-
-#       tree
-#     else
-#       puts Dir.pwd
-#     end}
-# end
-
 tree
-# puts Dir.glob('./**')
-
-# begin
-
-# rescue ArgumentError
-#     p true  # or, p "ArgumentError raised!", or whatever you'd like here
-# end
