936d2a50c960f919f6fd80ccf2fe1b5449d64c16
Ben Morimoto & Nikolai Calder <pair+bkmorimoto.nikocal@devbootcamp.com>
2015-03-17 15:41:57 -0400

    added global argument piece

--- tree.rb
+++ tree.rb
@@ -3,12 +3,14 @@ def list_directory(current_directory = '*', stack_level = 0)
   directory = Dir.glob(current_directory)
   directory.each do |f|
     puts
-    # puts "./#{f}"
     stack_level.times {print "  " }
-
     print File.basename(f)
     list_directory("#{f}/*", stack_level + 1)
    end
 end
 
-list_directory
+if ARGV[0].nil?
+  list_directory
+else
+  list_directory(ARGV[0].to_s)
+end
