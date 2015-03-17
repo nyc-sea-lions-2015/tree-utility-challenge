688c83aa8b2c42efe26d9027171c28092df516a7
Lucian Kahn <luciankahn@gmail.com>
2015-03-17 15:06:55 -0400

    complete release 2

--- tree.rb
+++ tree.rb
@@ -1,5 +1,18 @@
-def tree(directory)
-  p Dir[directory]
+# Release 1
+
+# def tree(directory)
+#   p Dir[directory]
+# end
+
+# tree('*')
+
+def tree(path)
+  Dir[path + '/*'].each do |sub_directory|
+    puts sub_directory
+    if File.directory?(sub_directory) # if it's a directory and not just a file
+      tree(sub_directory)
+    end
+  end
 end
 
-tree('*')
+tree('.')
